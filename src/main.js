ff307d0cf7ad41617df0994d0b14ac89b180b9e2
NoraMac <cjz741262008cjz@gmail.com>
2018-08-14 00:02:35 +0800

    use vue-analytics

--- src/main.js
+++ src/main.js
@@ -1,6 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
 
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+import VueAnalytics from 'vue-analytics'
+
+Vue.use(VueAnalytics,{
+    id () {
+    return 'UA-474915333-5'
+    },
+    autoTracking:{
+        screenview: true
+    },
+    router
+})
 Vue.config.productionTip = false
 
 new Vue({
